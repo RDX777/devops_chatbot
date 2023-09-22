6c152895d073c9f6c226779f5ae6a38c9fd51237
Anderson Tome <RDX_7777@yahoo.com.br>
2023-09-22 15:18:18 -0300

    deletado arquivos

--- restart_all_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker-compose -f ./redis/docker-compose.yml restart
-docker-compose -f ./mongodb/docker-compose.yml restart
-docker-compose -f ./postgres/docker-compose.yml restart
-docker-compose -f ./evolution-api/docker-compose.yml restart
-docker-compose -f ./chatwoot/docker-compose.yml restart
-docker-compose -f ./typebot.io/docker-compose.yml restart
-docker-compose -f ./n8n/docker-compose.yml restart
-docker-compose -f ./nginx/docker-compose.yml restart
